bbdbce18b912b8241ae7de138e1bbe51bdd52c1d
Feysal <114604448+feysalomar@users.noreply.github.com>
2023-02-28 18:25:01 +0000

    added pages

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine','ejs');
+
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+
+
+app.get('/', (req, res) => {
+  var title = 'Home Page';
+  res.render('pages/index',{'title':title});
+});
+app.get('/about', (req, res) => {
+  var title = 'About Page';
+  res.render('pages/about',{'title':title});
+});
+app.get('/contacts', (req, res) => {
+  var title = 'Contacts Page';
+  res.render('pages/contacts',{'title':title});
+});
+app.get('/projects', (req, res) => {
+  var title = 'Projects Page';
+  res.render('pages/projects',{'title':title});
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
