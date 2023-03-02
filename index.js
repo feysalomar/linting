c611e1577e38e60a65c7a85da776176467bdfce1
Feysal <114604448+feysalomar@users.noreply.github.com>
2023-03-02 17:48:20 +0000

    update

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ app.set('view engine','ejs');
 
 //this will allow us to serve up static files, CSS, images & JS
 app.use(express.static(__dirname));
-
+//Heres our data for users
+var data = require('./data/test.json');
 
 app.get('/', (req, res) => {
   var title = 'Home Page';
@@ -16,6 +17,30 @@ app.get('/about', (req, res) => {
   var title = 'About Page';
   res.render('pages/about',{'title':title});
 });
+
+
+//users index is our list page
+app.get('/users', function(req, res) {
+	var title = 'Users Page';
+	res.render('users/index', {
+    	title: title,
+    	users: data
+	});
+});
+//add user/view route - we are cheating by using the array index - 1
+app.get('/users/view/:id', function(req, res) {
+ var title = 'User Page';
+ var id = req.params.id;
+ res.render('users/view', {
+     title: title,
+     user: data[--id]
+ });
+});
+
+
+
+
+
 app.get('/contacts', (req, res) => {
   var title = 'Contacts Page';
   res.render('pages/contacts',{'title':title});
@@ -28,5 +53,6 @@ app.get('/projects', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+    console.log(data);
 });
 
